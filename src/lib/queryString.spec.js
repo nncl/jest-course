2e73d1aabf31a1b864ec49e1862190659a85aee0
Cauê Almeida <nncl@live.com>
2022-01-27 22:54:05 -0300

    feat: Testing array and object as params

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,6 +1,7 @@
 const { queryString } = require('./queryString');
 
 describe('Object to query string', () => {
+
   it('should create a valid query string when an object is provided', () => {
     const obj = {
       name: 'Caue',
@@ -11,4 +12,32 @@ describe('Object to query string', () => {
       'name=Caue&profession=developer',
     );
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Caue',
+      profession: 'developer',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe(
+      'name=Caue&profession=developer&abilities=JS,TDD',
+    );
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Caue',
+      profession: 'developer',
+      abilities: {
+        first: 'JS',
+        second: 'TDD'
+      }
+    };
+
+    expect(() => {
+      queryString(obj)
+    }).toThrowError();
+  });
+
 });
