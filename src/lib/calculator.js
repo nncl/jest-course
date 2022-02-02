a9d4a6b141b4ecb2221d8c586531eabfdf80fd7a
Cauê Almeida <nncl@live.com>
2022-02-01 23:18:16 -0300

    refactor: Importing/exporting via es6 modules

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,4 +1,4 @@
-module.exports.sum = (num1, num2) => {
+export function sum (num1, num2) {
   const int1 = parseInt(num1, 10); // +num1 transform an empty string to 0
   const int2 = parseInt(num2, 10);
 
--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -6,9 +6,9 @@ const keyValueToString = ([key, value]) => {
   return `${key}=${value}`;
 };
 
-module.exports.queryString = obj => Object.entries(obj).map(keyValueToString).join('&');
+export const queryString = obj => Object.entries(obj).map(keyValueToString).join('&');
 
-module.exports.parse = string => Object.fromEntries(
+export const parse = string => Object.fromEntries(
   string.split('&').map(item => {
     let [key, value] = item.split('=');
 
