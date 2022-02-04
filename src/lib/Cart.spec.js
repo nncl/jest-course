0eba6049ec0782cab5aa64e8ffd206409967c75c
Cauê Almeida <nncl@live.com>
2022-02-03 23:29:36 -0300

    feat(cart): Updating total when a product is removed

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -2,9 +2,15 @@ import Cart from './Cart';
 
 describe(`Cart`, () => {
   let cart;
+
   let product = {
     title: `Adidas running shoes - men`,
     price: 35388, // 353.88 | R$ 353,88
+  };
+
+  let product2 = {
+    title: `Adidas running shoes - women`,
+    price: 45166, // 451.66 | R$ 451,66
   };
 
   beforeEach(() => {
@@ -38,5 +44,21 @@ describe(`Cart`, () => {
     });
 
     expect(cart.getTotal()).toEqual(35388);
+  });
+
+  it(`should update total when a product gets included and then removed`, () => {
+    cart.add({
+      product,
+      quantity: 2, // 70776
+    });
+
+    cart.add({
+      product: product2,
+      quantity: 1, // 70776
+    });
+
+    cart.remove(product);
+
+    expect(cart.getTotal()).toEqual(45166);
   });
 });
