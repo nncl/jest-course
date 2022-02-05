de255b3fcd9e430d997389d0939b0e4840c41d2e
Cauê Almeida <nncl@live.com>
2022-02-04 23:01:23 -0300

    feat(cart): Describing getTotal

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -17,48 +17,50 @@ describe(`Cart`, () => {
     cart = new Cart();
   });
 
-  it(`should return 0 when getTotal() is executed in a newly created instance`, () => {
-    expect(cart.getTotal()).toEqual(0);
-  });
-
-  it(`should multiply quantity and price and receive the total amount`, () => {
-    const item = {
-      product,
-      quantity: 2, // 70776
-    };
-
-    cart.add(item);
-
-    expect(cart.getTotal()).toEqual(70776);
-  });
-
-  it(`should ensure no more than one product exists at a time`, () => {
-    cart.add({
-      product,
-      quantity: 2, // 70776
+  describe('getTotal()', () => {
+    it(`should return 0 when getTotal() is executed in a newly created instance`, () => {
+      expect(cart.getTotal()).toEqual(0);
     });
 
-    cart.add({
-      product,
-      quantity: 1, // 35388
+    it(`should multiply quantity and price and receive the total amount`, () => {
+      const item = {
+        product,
+        quantity: 2, // 70776
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(70776);
     });
 
-    expect(cart.getTotal()).toEqual(35388);
-  });
+    it(`should ensure no more than one product exists at a time`, () => {
+      cart.add({
+        product,
+        quantity: 2, // 70776
+      });
 
-  it(`should update total when a product gets included and then removed`, () => {
-    cart.add({
-      product,
-      quantity: 2, // 70776
+      cart.add({
+        product,
+        quantity: 1, // 35388
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
     });
 
-    cart.add({
-      product: product2,
-      quantity: 1, // 70776
+    it(`should update total when a product gets included and then removed`, () => {
+      cart.add({
+        product,
+        quantity: 2, // 70776
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1, // 70776
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(45166);
     });
-
-    cart.remove(product);
-
-    expect(cart.getTotal()).toEqual(45166);
   });
 });
